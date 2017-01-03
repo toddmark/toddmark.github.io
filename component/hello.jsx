19486c60db4cc30db59f4edc4e334a4b2488350d
Todd Mark <renqingsong@ezbuy.com>
2017-01-03 19:17:00 +0800

    add Nav component next: add bootstrap

--- component/hello.jsx
+++ component/hello.jsx
@@ -1,12 +1,14 @@
 require('./hello.less')
 
 import React, {Component} from 'react';
+import Nav from './navbar/index.jsx';
 
 class Hello extends Component{
 
   render() {
     return (
       <div>Boot Strap
+        <Nav />
         <p className="">
           <img src={require('./witcher.jpeg')} alt=""/>
         </p>
---
+++ component/navbar/index.jsx
@@ -0,0 +1,12 @@
+import React, {Component} from 'react';
+export default class Nav extends Component{
+  render() {
+    return(
+      <ul className="nav nav-tabs">
+        <li>Home</li>
+        <li>page</li>
+        <li>About</li>
+      </ul>
+    )
+  }
+}
