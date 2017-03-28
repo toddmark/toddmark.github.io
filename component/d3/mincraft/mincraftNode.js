9f36e0a732509bb172d8d34b41189a9d8c4379d1
Todd Mark <toddmark@163.com>
2017-03-28 23:25:30 +0800

    update mincraft node

--- component/d3/mincraft/mincraftNode.js
+++ component/d3/mincraft/mincraftNode.js
@@ -1,9 +1,18 @@
 const node = document.createElement('div');
 
+const width = 1000;
+const height = 750;
+const rectSize = 50;
+const x = width / rectSize;
+const y = height / rectSize;
 const stage = d3.select(node).append('svg');
-stage
-  .attr('width', '1000').attr('height', '750').attr("transform", "translate(20,20)")
-  .style('background', '#eee')
+stage.attr('width', width).attr('height', 750).style('background', '#eee')
+stage.selectAll('rect').data(d3.range( x * y))
+  .enter().append('rect').attr('transform', translate).attr('width', rectSize).attr('height', rectSize).style('stroke', '#000').style('fill', '#fff')
+
+function translate(d) {
+  return `translate(${ d % x * rectSize },${ Math.floor(d / x) * rectSize })`
+}
 
 module.exports = {
   node
