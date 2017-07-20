ef3318b3e5c117895a02c80a5d20e93961111835
任青松(｡◕∀◕｡) <renqingsong@ezbuy.com>
2017-07-20 20:58:26 +0800

    finish utility

--- component/index.jsx
+++ component/index.jsx
@@ -1,9 +1,13 @@
-import React, { Component } from 'react';
+// eslint-disable-next-line no-unused-vars
+import React, { Component } from 'react';  
 import Particles from 'react-particles-js';
 import { Link } from 'react-router';
 import particlesProps from './particleProps.js';
 import './index.less';
 import Unsplash from 'unsplash-js';
+
+// utility
+import Carousel from './utility/carousel.jsx';
 
 const unsplash = new Unsplash({
   applicationId: 'f8b303ea98159bfa3b94f81308eec8dc020f89bcd898f8502f41e6b1bbabd40d',
@@ -46,30 +50,7 @@ export default class Index extends Component {
           <Link to="/hello">Mill</Link>
         </div>
         <div className="flex-parent">
-          <div id="carousel-example-generic" className="flex-center-div carousel slide" data-ride="carousel">
-            <ol className="carousel-indicators">
-              <li data-target="#carousel-example-generic" data-slide-to="0" className="active"></li>
-              <li data-target="#carousel-example-generic" data-slide-to="1"></li>
-              <li data-target="#carousel-example-generic" data-slide-to="2"></li>
-            </ol>
-            <div className="carousel-inner">
-              {this.state.imgArr.map((item, index) => (
-                <div key={index} className={index === 0 ? 'item active' : 'item'}>
-                  <img className="img" src={item} alt="..." />
-                  <div className="carousel-caption"> ... </div>
-                </div>
-              ))}
-            </div>
-            <a className="left carousel-control" href="#carousel-example-generic" role="button" data-slide="prev">
-              <span className="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
-              <span className="sr-only">Previous</span>
-            </a>
-            <a className="right carousel-control" href="#carousel-example-generic" role="button" data-slide="next">
-              <span className="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
-              <span className="sr-only">Next</span>
-            </a>
-          </div>
-
+          Carousel imgArr={this.state.imgArr} />  
         </div>
         <Particles
           style={{ position: 'absolute', left: 0, top: 0, zIndex: -2 }}
