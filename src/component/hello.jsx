021588d00a3237ddd5b769ff573450e0a4096ef7
toddmark <toddmark0315@gmail.com>
2017-08-10 23:27:34 +0800

    update

--- src/component/hello.jsx
+++ src/component/hello.jsx
@@ -1,20 +1,20 @@
-require('./hello.less')
+require('./hello.less');
 
-import {TweenMax,TimelineLite, TimelineMax} from 'gsap'
-import ScrollMagic from 'scrollmagic'
-require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap')
+import { TweenMax, TimelineLite, TimelineMax } from 'gsap';
+import ScrollMagic from 'scrollmagic';
+require('scrollmagic/scrollmagic/uncompressed/plugins/animation.gsap');
 
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import Nav from './navbar';
 
-export default class Hello extends Component{
+export default class Hello extends Component {
 
   constructor(props) {
-    super(props)
+    super(props);
     this.state = {
       text: 'This is test page.',
       title: 'what you see is what you get, is that true? See, Not ture. Can not figure it. I will crazy about it.'
-    }
+    };
   }
 
   componentDidMount() {
@@ -23,30 +23,30 @@ export default class Hello extends Component{
     // create a scene
     const spanArray = document.getElementById('stick').getElementsByTagName('span');
     var laola = new TimelineMax()
-      .add(TweenMax.staggerTo(spanArray, 0.25, {y: -20, x: -15, scale: 1.2, color: '#667' }, 0.1))
-      .add(TweenMax.staggerTo(spanArray, 0.25, {y: 0, x: 0, scale: 1, color: '#667'}, 0.1), 0.25);
+      .add(TweenMax.staggerTo(spanArray, 0.25, { y: -20, x: -15, scale: 1.2, color: '#667' }, 0.1))
+      .add(TweenMax.staggerTo(spanArray, 0.25, { y: 0, x: 0, scale: 1, color: '#667' }, 0.1), 0.25);
     new ScrollMagic.Scene({
-        duration: 1000,  // the scene should last for a scroll distance of 100px
-      })
-      .setPin("#stick") // pins the element for the the scene's duration
+      duration: 1000,  // the scene should last for a scroll distance of 100px
+    })
+      .setPin('#stick') // pins the element for the the scene's duration
       // .setTween(laola)
-      .setTween(spanArray, 0.5, {y: 30, opacity: 0.5})
+      .setTween(spanArray, 0.5, { y: 30, opacity: 0.5 })
       .addTo(this.controller);
   }
 
   render() {
     return (
-      <div id="wrapper" style={{height: 2000}}>
+      <div id="wrapper" style={{ height: 2000 }}>
         <Nav />
         <h1>{this.state.text}</h1>
-        <h5 style={{padding: 10, color: '#ccc'}}>please scroll down to the bottom page.</h5>
-        <p style={{fontSize: 80}}>some paragraph.</p>
-        <h3 id="stick" style={{padding: 10}}>{this.state.title.split('').map((item, key)=>{
+        <h5 style={{ padding: 10, color: '#ccc' }}>please scroll down to the bottom page.</h5>
+        <p style={{ fontSize: 80 }}>some paragraph.</p>
+        <h3 id="stick" style={{ padding: 10 }}>{this.state.title.split('').map((item, key) => {
           item = item === ' ' ? '\u00A0' : item;
-          return (<span style={{display: 'inline-block'}} key={key}>{item}</span>);
+          return (<span style={{ display: 'inline-block' }} key={key}>{item}</span>);
         })}</h3>
-        <h1 className={"text-info"} style={{textAlign: 'center'}}>End</h1>
+        <h1 className={'text-info'} style={{ textAlign: 'center' }}>End</h1>
       </div>
-    )
+    );
   }
 }
